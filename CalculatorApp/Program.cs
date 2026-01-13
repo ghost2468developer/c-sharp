0abf5d346bbb04c5156802b08ff0aa0a3908921e
Kenneth Siyabonga Ncube <160495796+ghost2468developer@users.noreply.github.com>
2026-01-13 20:58:21 +0200

    feat(calculator): basic math methods

--- CalculatorApp/Program.cs
+++ CalculatorApp/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Calculator");
+        Console.Write("Enter first number: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter second number: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter operation (+, -, *, /): ");
+         string operation = Console.ReadLine();
+        double result = 0;
+        if (operation == "+")
+        {
+            result = num1 + num2;
+        }
+        else if (operation == "-")
+        {
+            result = num1 - num2;
+        }
+        else if (operation == "*")
+        {
+            result = num1 * num2;
+        }
+        else if (operation == "/")
+        {
+            if (num2 == 0)
+            {
+                Console.WriteLine("Error: Cannot divide by zero.");
+                return;
+            }
+
+            result = num1 / num2;
+        }
+        else
+        {
+            Console.WriteLine("Invalid operation.");
+            return;
+        }
+        Console.WriteLine($"Result: {result}");
+    }
+}
--- CalculatorApp/obj/Debug/net10.0/CalculatorApp.AssemblyInfo.cs
+++ CalculatorApp/obj/Debug/net10.0/CalculatorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abeae201d5306f6880f0fe8ae7a320f5e9e3bf41")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
