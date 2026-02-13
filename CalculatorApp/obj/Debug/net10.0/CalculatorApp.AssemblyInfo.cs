5b30d5b0392aba7277bbebd66402ae6c3faed83a
Kenneth Siyabonga Ncube <160495796+ghost2468developer@users.noreply.github.com>
2026-02-13 13:59:34 +0200

    Update CalculatorApp.AssemblyInfo.cs

--- CalculatorApp/obj/Debug/net10.0/CalculatorApp.AssemblyInfo.cs
+++ CalculatorApp/obj/Debug/net10.0/CalculatorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ed75ab3129de7165d7f2c25044ff03c4b3b17d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b01cb8a291ef881759d91ecd015d20cce896c7b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculatorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
